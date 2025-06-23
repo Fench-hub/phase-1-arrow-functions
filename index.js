7cb0a92e1ecf88149af159ef16e8e2ce2f3c69af
felona jepchumba <felonajepchumba@gmail.com>
2025-06-23 07:13:08 +0300

    implement divide,square,add and functions using fuction expressions and arrow functions

---
+++ index.js
@@ -0,0 +1,8 @@
+//function expression called divide
+const divide=function() {
+    return 2000 / 100;
+};
+//arrow function called square
+const square = x => x * x;
+//arrow function called add
+const add = (a, b) => a + b;
